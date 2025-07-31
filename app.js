107f3e96982a920311c5567153eaf5ae19db808f
omarseddati16 <omarseddati16@gmail.com>
2025-07-31 13:57:43 +0200

    fine

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 const port = 3000
 
-app.use(express.static(''))
+app.use(express.static('fake'))
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog');
@@ -12,37 +12,36 @@ app.get('/', (req, res) => {
 
 const posts = [
   {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tags: 
+    titolo: "post 1",
+    contenuto: "primo post",
+    immagine: "/Quadrato-nero.png",
+    tags: ["vuoto", "vuoto"]
   },
   {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tags: 
+    titolo: "post 2",
+    contenuto: "secondo post",
+    immagine: "/Quadrato-nero.png",
+    tags: ["vuoto", "vuoto"]
   },
   {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tags: 
+    titolo: "post 3",
+    contenuto: "terzo post",
+    immagine: "/Quadrato-nero.png",
+    tags: ["vuoto", "vuoto"]
   },
   {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tags: 
+    titolo: "post 4",
+    contenuto: "quarto post",
+    immagine: "/Quadrato-nero.png",
+    tags: ["vuoto", "vuoto"]
   },
   {
-    titolo: "",
-    contenuto: "",
-    immagine: "",
-    tags: 
+    titolo: "post 5",
+    contenuto: "quinto post",
+    immagine: "/Quadrato-nero.png",
+    tags: ["vuoto", "vuoto"]
   }
 ]
-
 
 app.get('/bacheca', (req, res) => {
   res.json(posts);
